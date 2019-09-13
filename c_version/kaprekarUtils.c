d3931fed2548e22b990c05aa417822c43825b500
Aryan Timla <aryan.timla@huskers.unl.edu>
2019-09-13 00:01:21 -0500

    Update kaprekarUtils.c

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -34,11 +34,14 @@ int isKaprekar(int n)
 
   int i;
   //compute the square
-  long square = n * (long) n;
+  long square = n * (long)n;
   //compute the number of digits of the square
-  int numDigits = (int) log10(n) + 1;
-  long modulus = 0;
+  //changed n to square
+  int numDigits = (int)log10(square) + 1;
+  //changed the modulus from 0 to 1
+  long modulus = 1;
   long first, second;
+
 
   //for each possible "split" of the square...
   for(i=1; i<=numberOfDigits; i++) {
