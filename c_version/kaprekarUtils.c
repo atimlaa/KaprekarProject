61d11a327a86dc96669d74839ae70ce9f5fa4e53
atimlaa <54657323+atimlaa@users.noreply.github.com>
2019-09-13 13:17:07 -0500

    Update kaprekarUtils.c

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -33,12 +33,14 @@ int isKaprekar(int n) {
   //compute the square
   long square = n * (long) n;
   //compute the number of digits of the square
-  int numDigits = (int) log10(n) + 1;
+  //change n to square
+  int numDigits = (int) log10(square) + 1;
   long modulus = 0;
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  // change numberOfDigits to numDigits
+  for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
     modulous *= 10;
 
